be471879ee3b31a43f2028834a860a493f0185fc
Eduardo Hoths <desenvolvimento@nulogistica.com.br>
2024-06-19 18:09:27 -0300

    update

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,17 +1,33 @@
+import { Image, StyleSheet } from "react-native";
 import { Stack, useLocalSearchParams } from "expo-router";
 import { Text, View } from "../../../components/Themed";
 
-import { Fontisto } from "@expo/vector-icons";
 import React from "react";
+import { defaultPizzaImage } from "@/src/components/ProductListItem";
+import products from "@/assets/data/products";
 
 const product = () => {
   const { id } = useLocalSearchParams();
+  const product = products.find((p) => p.id === Number(id));
+
+  if (!product) return <Text> Product not found!</Text>;
+
   return (
     <View>
-      <Stack.Screen options={{ title: `Details: ${id}` }} />
-      <Text style={{ fontSize: 20, marginTop: 50}}>Product Details Page {id}</Text>
+      <Stack.Screen options={{ title: `${product.name}` }} />
+
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={{ width: "100%", aspectRatio: 1 }}
+      />
+
+      <Text style={{ fontSize: 20, marginTop: 50 }}>
+        Product Details Page {id}
+      </Text>
     </View>
   );
 };
 
 export default product;
+
+const styles = StyleSheet.create({});
