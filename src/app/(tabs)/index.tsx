9c0ea3b3d7a55b24c0fb55078286ebcf3b1b2320
Eduardo Hoths <desenvolvimento@nulogistica.com.br>
2024-06-19 16:58:27 -0300

    Flatlist

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,11 +1,18 @@
+import { FlatList, View } from "react-native";
+
 import ProductListItem from "@/src/components/ProductListItem";
-import { View } from "react-native";
 import products from "@/assets/data/products";
 
 export default function MenuScreen() {
   return (
     <View>
-      <ProductListItem product={products[5]} />
+      <FlatList
+        data={products}
+        renderItem={({ item }) => <ProductListItem product={item} />}
+        numColumns={2}
+        contentContainerStyle={{ gap: 10, padding: 10 }}
+        columnWrapperStyle={{ gap: 10 }}
+      />
     </View>
   );
 }
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -3,16 +3,20 @@ import { Image, StyleSheet, Text, View } from "react-native";
 import Colors from "../constants/Colors";
 import { Product } from "../types";
 
-export const defaultPizzaImage = "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png"
+export const defaultPizzaImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
 
 type ProductListItemProps = {
-  product: Product
-}
+  product: Product;
+};
 
 const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: product.image || defaultPizzaImage }} style={styles.image} />
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+      />
 
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
@@ -27,6 +31,8 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     padding: 10,
     borderRadius: 20,
+    flex: 1,
+    maxWidth: "50%",
   },
   title: {
     fontSize: 20,
