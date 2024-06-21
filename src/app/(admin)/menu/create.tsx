d421c9fc22093d626bc926137899b69efeeb1228
Eduardo Hoths <desenvolvimento@nulogistica.com.br>
2024-06-21 12:28:02 -0300

    Create product

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -1,23 +1,97 @@
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+
+import { Image, StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useState } from "react";
 
 import Button from "@/src/components/Button";
-import React from "react";
+import Colors from "@/src/constants/Colors";
+import { Stack } from "expo-router";
+import { defaultPizzaImage } from "@/src/components/ProductListItem";
 
 const CreateProductScreen = () => {
-  const onCreate = () => {};
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [errors, setErrors] = useState("");
+  const [image, setImage] = useState<null | string>(null);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  const resetFields = () => {
+    setName("");
+    setPrice("");
+  };
+
+  const validadeInput = () => {
+    setErrors("");
+    if (!name) {
+      setErrors("Name is required");
+      return false;
+    }
+    if (!price) {
+      setErrors("Price is required");
+      return false;
+    }
+
+    if (isNaN(parseFloat(price))) {
+      setErrors("Price is not a number");
+      return false;
+    }
+
+    return true;
+  };
+
+  const onCreate = () => {
+    if (!validadeInput()) {
+      return;
+    }
+    resetFields();
+  };
 
   return (
     <View style={styles.container}>
+      <Stack.Screen options={{
+        title: "Create Product"
+      }}/>
+      
+      <Image
+        style={styles.image}
+        source={{ uri: image || defaultPizzaImage }}
+      />
+      <Text onPress={pickImage} style={styles.textButton}>
+        Select image
+      </Text>
+
       <Text style={styles.label}>Name</Text>
-      <TextInput placeholder="Name" style={styles.input} />
+      <TextInput
+        placeholder="Name"
+        style={styles.input}
+        value={name}
+        onChangeText={setName}
+      />
 
       <Text style={styles.label}>Price ($)</Text>
       <TextInput
         placeholder="9.99"
         style={styles.input}
         keyboardType="numeric"
+        value={price}
+        onChangeText={setPrice}
       />
 
+      <Text style={{ color: "red" }}>{errors}</Text>
       <Button text="Create" onPress={onCreate} />
     </View>
   );
@@ -31,6 +105,11 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     padding: 10,
   },
+  image: {
+    width: "50%",
+    aspectRatio: 1,
+    alignSelf: "center",
+  },
   input: {
     backgroundColor: "white",
     padding: 10,
@@ -41,5 +120,11 @@ const styles = StyleSheet.create({
   label: {
     color: "gray",
     fontSize: 16,
+  },
+  textButton: {
+    alignSelf: "center",
+    fontWeight: "bold",
+    color: Colors.light.tint,
+    marginVertical: 10,
   },
 });
